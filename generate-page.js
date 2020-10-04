9da8174d88c57a1192a0de1fc732d406a395906e
test <naminukas@kikaitachi.com>
2020-10-04 14:56:09 +0100

    Update sorting

--- generate-page.js
+++ generate-page.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const amazonLinks = [
   'https://www.amazon.co.uk/s?k=hotter+shoes&i=shoes&rh=n%3A1769798031%2Cp_89%3AHotter%2Cp_n_size_browse-vebin%3A1919984031%7C1919985031%7C1919986031%7C1919987031%7C1919988031%7C1919989031%7C1919990031&s=price-asc-rank&dc&qid=1601812569&rnid=1918531031&ref=sr_nr_p_n_size_browse-vebin_1',
-  'https://www.amazon.co.uk/s?k=irregular+choice+shoes&i=shoes&rh=n%3A1769798031%2Cp_89%3AIrregular+Choice%2Cp_n_size_browse-vebin%3A1919985031%7C1919986031%7C1919988031%7C1919990031&dc&qid=1601812708&rnid=1918531031&ref=sr_nr_p_n_size_browse-vebin_4'
+  'https://www.amazon.co.uk/s?k=irregular+choice+shoes&i=shoes&rh=n%3A1769798031%2Cp_89%3AIrregular+Choice%2Cp_n_size_browse-vebin%3A1919985031%7C1919986031%7C1919988031%7C1919990031&s=price-asc-rank&dc&qid=1601819630&rnid=1918531031&ref=sr_st_price-asc-rank'
 ];
 
 console.log('<!DOCTYPE html>');
