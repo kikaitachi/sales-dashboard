6831edc46d35c920c90a51ba52622770893ee86c
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2021-12-18 14:30:07 +0000

    Nicer style and less DOM

--- docs/main.js
+++ docs/main.js
@@ -1,20 +1,23 @@
+const selectedCategory = 'Shoes (women)';
+
 const renderSections = (data) => {
     const contentEl = document.getElementById("content");
     var content = "";
     for (const brand of data) {
-        content += `<div class="section">${brand.name}</div>`;
-        content += '<div class="sectionContent">';
-        for (const item of brand.items) {
-            content += '<div class="item">';
-            content += `<a href="https://www.amazon.co.uk/gp/product/${item.asin}/?tag=wishinfinit09-21"><img src="${item.imageUrl}" style="height:122px;"></a>`;
-            content += '<br/>';
-            content += item.name;
-            content += '<br/>';
-            content += "from £";
-            content += item.price;
+        const items = brand.items;
+        if (items.length != 0) {
+            //if (brand.category == selectedCategory) {
+            content += `<div class="section">${brand.name}</div>`;
+            content += '<div class="sectionContent">';
+            for (const item of brand.items) {
+                content += `<a class="item" href="https://www.amazon.co.uk/gp/product/${item.asin}?tag=wishinfinit09-21">`;
+                content += `<img src="${item.imageUrl}">`;
+                content += `<div>${item.name}</div>`;
+                content += `from <b>£${item.price}</b>`;
+                content += '</a>';
+            }
             content += '</div>';
         }
-        content += '</div>';
     }
     contentEl.innerHTML = content;
 }
